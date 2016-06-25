8e210df632680afa793f4cb7769df73d2114cc5b
kernel-killer <danosimko1@gmail.com>
2016-06-25 14:32:44 +0200

    TWRP now uses ToyBox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,7 @@ TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p7"
 TW_CRYPTO_MNT_POINT := "/data"
 TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_USE_TOOLBOX := true
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_FB2PNG := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
